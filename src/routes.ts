82d0c40a15380ae2e400a6aa21f98a27eca08523
Andre LFA <andrelfaareas@gmail.com>
2020-08-08 14:16:35 -0300

    Creates the database filters

--- src/routes.ts
+++ src/routes.ts
@@ -5,6 +5,7 @@ import ClassesController from './controllers/ClassesController';
 const routes = express.Router();
 const classController = new ClassesController;
 
+routes.get('/classes', classController.index); 
 routes.post('/classes', classController.create); 
 
 export default routes;
